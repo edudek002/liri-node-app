decbe13607d1ae4d73702b19b108d236f4ad9f54
Edyta <dudek002@hotmail.com>
2018-02-08 16:41:52 -0600

    all fixed

--- liri.js
+++ liri.js
@@ -10,17 +10,17 @@ var Spotify = require('node-spotify-api');
 
 var fs = require("fs");
 
+var nodeArgs = process.argv;
+
 var userInput1 = process.argv[2];
 
 var userInput2 = process.argv[3];
 
-var userInput3 = process.argv[4];
-
-var userInput4 = process.argv[5];
-
-
-
 var tweetsText=[];
+
+var newSong;
+
+//var words=[];
 
 var text="";
 
@@ -46,7 +46,7 @@ switch(userInput1){
 	break;
 
 	case "do-what-it-says":
-	console.log("You said ...");
+	console.log("\n" + "My TEXT IS BELOW:");
 	console.log("====================");
 	getText();
 	break;
@@ -83,7 +83,6 @@ function getSpotify(){
 	var spotify = new Spotify(keys.spotify);
 
     //Name of the new song
-	var newSong;
 
 	if(userInput2 === undefined){
 		newSong='The Sign';
@@ -91,8 +90,18 @@ function getSpotify(){
 	}
 
 	else {
-		newSong = userInput2;
-		//console.log(newSong);
+		// Loop through all the words in the node argument
+
+		for (var i = 3; i < nodeArgs.length; i++) {
+
+		  if (i > 3 && i < nodeArgs.length) {
+
+		    newSong = newSong + "+" + nodeArgs[i];
+		  }
+		  else {
+		    newSong = userInput2;
+		  }
+		}
 	}
 	 
 	spotify.search({ type: 'track', query: newSong}, function(err, data) {
@@ -100,7 +109,7 @@ function getSpotify(){
 	    return console.log('Error occurred: ' + err);
 	  }
 	 
-	console.log("Getting information about song: " + newSong);
+	console.log("\n" + "Getting information about song: " + data.tracks.items[0].name);
 	
 	//Information about the song:
 	//console.log(data.tracks.items[0]);
@@ -117,34 +126,9 @@ function getSpotify(){
 
 }
 
-
-
 //Function to print information about the movie
 
 function getMovie(){
-
-//	var movieName = "";
-
-// Loop through all the words in the node argument
-// And do a little for-loop magic to handle the inclusion of "+"s
-/*for (var i = 3; i < nodeArgs.length; i++) {
-
-  if (i > 3 && i < nodeArgs.length) {
-
-    movieName = movieName + "+" + nodeArgs[i];
-
-  }
-
-  else {
-
-    movieName += nodeArgs[i];
-
-  }
-}
-*/
-
-
-	var nodeArgs = process.argv;
 
 	var newMovie ="";
 
@@ -155,24 +139,20 @@ function getMovie(){
 	else {
 		// Loop through all the words in the node argument
 
+		newMovie = userInput2;
+
 		for (var i = 3; i < nodeArgs.length; i++) {
 
 		  if (i > 3 && i < nodeArgs.length) {
 
 		    newMovie = newMovie + "+" + nodeArgs[i];
-
 		  }
-
 		  else {
-
 		    newMovie = userInput2;
-
 		  }
 		}
-
 	}
 
-	
 	// Request to the OMDB API with the movie specified
 	var queryUrl = "http://www.omdbapi.com/?t=" + newMovie + "&y=&plot=short&apikey=trilogy";
 
@@ -187,67 +167,122 @@ function getMovie(){
 	  	console.log("Getting information about movie: " + JSON.parse(body).Title);
 
 	    // Parse the body of the site 
+	    
 	    console.log("\n" +"Release Year: " + JSON.parse(body).Year);
+	  	console.log("================================="); 
 	  	console.log("\n" +"Title: " + JSON.parse(body).Title);
+	  	console.log("================================="); 
 	  	console.log("\n" +"IMDB rating: " + JSON.parse(body).imdbRating);
+	  	console.log("================================="); 
 	  	console.log("\n" +"Rotten Tomatoes Rating: " + JSON.parse(body).tomatoerating);
+	  	console.log("================================="); 
 	  	console.log("\n" +"Country produced: " + JSON.parse(body).Country);
+	  	console.log("================================="); 
 	  	console.log("\n" +"Language of the movie: " + JSON.parse(body).Language);
+	  	console.log("================================="); 
 	  	console.log("\n" +"Plot: " + JSON.parse(body).Plot);
+	  	console.log("================================="); 
 	  	console.log("\n" +"Actors: " + JSON.parse(body).Actors);
+	  	console.log("================================="); 
 	  }
 	});
 
 }
-
+//Fuction that gets text from random.txt file
 function getText(){
+ 
+ 
+ 	// The code will store the contents of the reading inside the variable "data"
+ 	fs.readFile("random.txt", "utf8", function(error, data) {
+ 
+	 	// If the code experiences any errors it will log the error to the console.
+	 	if (error) {
+	 	    return console.log(error);
+	 	}
+	 
+	 	//Print the contents of data
+	 	console.log(data);
+	 
+	 	//Split it by commas (to make it more readable)
+	 	var dataArr = data.split(",");
+	 
+	 	//GET THE SONG FROM RANDOM.TXT  
 
+	 	var str1 = dataArr[1];
+		var newStr = str1.match(/(?:"[^"]*"|^[^"]*$)/)[0].replace(/"/g, "");
+		var songWords = newStr.split(" ");
+	 	newSong="";
+	 	  
+	 	for (var i = 0; i < songWords.length; i++) {
 
-	// The code will store the contents of the reading inside the variable "data"
-	fs.readFile("random.txt", "utf8", function(error, data) {
+			newSong = newSong + "+" + songWords[i];
+		}
 
-	  // If the code experiences any errors it will log the error to the console.
-	  if (error) {
-	    return console.log(error);
-	  }
+		//console.log("\n" + "We now check this song: " + newSong);
+	 
+	 	userInput2 = newSong;
 
-	  // We will then print the contents of data
-	  console.log(data);
+	 	getSpotify();
 
-	  // Then split it by commas (to make it more readable)
-	  var dataArr = data.split(",");
+	 	text = dataArr[1];
 
-	  // We will then re-display the content as an array for later use.
-	  console.log(dataArr);
-	  console.log("Check this song: " + dataArr[1]);
+	 	writeText();
 
-	  newSong = dataArr[1];
+	 	//GET THE MOVIE FROM RANDOM.TXT
 
-	  text = newSong;
+	 	var str2 = dataArr[3];
+		var veryNewStr = str2.match(/(?:"[^"]*"|^[^"]*$)/)[0].replace(/"/g, "");
+		var movieWords = veryNewStr.split(" ");
+	 	newMovie="";
+	 	  
+	 	for (var i = 0; i < movieWords.length; i++) {
 
-	  writeText();
+			newMovie = newMovie + "+" + movieWords[i];
+		}
 
+		console.log("\n" + "We now check this movie: " + newMovie);
+	 
+	 	userInput2 = newMovie; 
 
-	});
+	 	getMovie();
 
-}
+	 	text = dataArr[3]; 
+
+ 		writeText();
+ 
+ 	});
+ 
+ }
 
 function writeText() {
 
-	fs.writeFile("log.txt", text, function(err) {
+	fs.appendFile("log.txt", text, function(err) {
 
 	  // If the code experiences any errors it will log the error to the console.
 	  if (err) {
 	    return console.log(err);
 	  }
 
-	  // Otherwise, it will print: "movies.txt was updated!"
-	  console.log("log.txt was updated!");
+	  console.log("\n" + "I posted information below in log.txt file!" + "\n");
 
 	});
 
 }
+/*
+fs.appendFile(textFile, "Hello Kitty", function(err) {
 
+  // If an error was experienced we say it.
+  if (err) {
+    console.log(err);
+  }
+
+  // If no error is experienced, we'll log the phrase "Content Added" to our node console.
+  else {
+    console.log("Content Added!");
+  }
+
+});
+*/
 
 
 
